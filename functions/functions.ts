4e65ac643f79df02e3cf06fb2b47f49e997010dc
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2023-11-03 23:04:40 +0530

    funtions

--- functions/functions.ts
+++ functions/functions.ts
@@ -1,71 +1,78 @@
-// function addTwo(num:number):number{
-//     //  num+2
-//      console.log("inside",num+2);
+/* 
+function- it is used to create a logic, 
+with the help of that logic we can do the same task multiple times 
+*/
 
-//      return num +2;
+/*
+syntax of the function
 
-// }
+function nameOfTheFuntion(parameters){
+    body Of The Function
+}
 
-// // addTwo(5)
-// console.log("outside",addTwo(5));
+nameOfThefuntion()        //function calling 
 
+*/
 
-// const timingsFunctionTwo = function () {
+function addTwo(num: number): number {
+    const output = num + 2;
+    console.log("inside", num + 2); //adding value(2) to the num and log the output
+    return num + 2; //here we are returning the output
+}
 
-// }
-
-// const timingsFunctionTHree = () => {  }
-
-let variable: string = "qwer";
-console.log(variable);
+addTwo(5) // function calling with argument value 5
+console.log("outside", addTwo(5)); // here log the returned output
 
 
 // Function defination
-/*function timings(somth_1: number, n_2: number, n_3: number, n_4: number = 0):number { // somth_1, n_2,n_3,n_4 these are parameters
+function timings(somth_1: number, n_2: number, n_3: number, n_4: number = 0): number { // somth_1, n_2,n_3,n_4 these are parameters
     console.log("somth_1", somth_1);
     console.log("n_2", n_2);
     console.log("n_3", n_3);
     console.log("n_4", n_4);
-    return 10;
+    return 10; // returning value 10
 }
 
 
 // Function calling
 const output = timings(1, 2, 3, 4);  //1,2,3,4 these are argunments
-console.log("output",output);
+console.log("output", output);
+/*
+1st parameter will take the 1st argument and it continue till last parameter in series.
+if one argument is  missed or not mentioned, then it gives result as undefined to the last parameter.
+we can also give default value to the parameter so that when no argument is given,
+then that default value will be taken or if argument is given then it considers that argument.
 
-
-function fullName(lastname:string,firstname:string){
-    console.log("lastname",lastname,"firstname",firstname);
-}
-
-
-
-fullName("adaka","Siva")
+incase argument is given if paramter is not there then argument will not be considerd
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function addFiveNumbers(n_1:number,n_2:number,_n3:number,_n4:number,_n5:number){
-    const add = n_1+n_2+_n3+_n4+_n5;
-
-    return add
-    
+function fullName(lastname: string, firstname: string) {
+    console.log("lastname", lastname, "firstname", firstname);
 }
 
-const result = addFiveNumbers(1,2,3,4,5);
+fullName("adaka", "Siva")
+
+
+
+function addFiveNumbers(n_1: number, n_2: number, _n3: number, _n4: number, _n5: number) {
+    const add = n_1 + n_2 + _n3 + _n4 + _n5;
+
+    return add
+
+}
+
+const result = addFiveNumbers(1, 2, 3, 4, 5);
 
 console.log(result);
+
+
+function fixedInfo(companyNmae:string, intime:string, outTime:string) {
+    return `${companyNmae}, ${intime}, ${outTime}`
+}
+
+
+function variableInfo(reusefun, employId:number, employName:string, department:string, salary:number) {
+    console.log(`${reusefun}${employId} ${employName} ${department} ${salary} `);
+}
+
+variableInfo(fixedInfo("kia", "9:30", "5:30"), 2266, "Siva", "steering", 12000)
