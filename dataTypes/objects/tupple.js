6f296adb4543ce8e2cfe4d57db3fcf65b964e6db
adakasnr <adakasnr@gmail.com>
2023-11-09 23:07:47 +0530

    tupple

---
+++ dataTypes/objects/tupple.js
@@ -0,0 +1,10 @@
+// tupple : tupple function is used in arrays and to write datatype to elements 
+var educationalDetails;
+educationalDetails = ["Btech", 71, true]; // 
+console.log(educationalDetails);
+var sales;
+sales = [123, 124, 125, 126]; // Error : index of the array must be followed
+console.log(sales);
+var naga;
+naga = ["Siva", "phani@gmail"]; // Error : datatype is not matching
+console.log(naga);
---
+++ dataTypes/objects/tupple.ts
@@ -0,0 +1,17 @@
+// tupple : tupple function is used in arrays and to write datatype to elements 
+
+let educationalDetails : [string, number, boolean]
+
+educationalDetails = ["Btech", 71, true] // 
+console.log(educationalDetails);
+
+let sales:[number,number,number]
+
+sales =[123,124,125,126] // Error : index of the array must be followed
+console.log(sales);
+
+
+let naga:[string,number]
+
+naga =["Siva","phani@gmail"] // Error : datatype is not matching
+console.log(naga);
